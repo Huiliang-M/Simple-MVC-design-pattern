cfd00b7f007c78286395c3095f4099323745c3a6
Huiliang Huang <huilianghuang2010@yahoo.com>
2017-06-23 14:27:26 -0700

    Add files via upload

---
+++ PaSkCode/Particle.java
@@ -0,0 +1,15 @@
+package PaSkCode;
+
+public class Particle{
+
+    int px,py,velX,velY,radius;
+    
+    Particle(int radius, int x, int y, int velX, int velY){
+        
+        this.radius = radius;
+        this.px = x;
+        this.py = y;
+        this.velX = velX;
+        this.velY = velY;
+    }
+}
